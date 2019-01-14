fb90de9af14219db28b2eb7e18df312f874b349e
yuri kurokawa <mp963@ezweb.ne.jp>
2019-01-15 00:44:30 +0900

    add_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,10 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
+    def show
+      @user = User.find(params[:id])
+    end
+
     def update
       user = User.find(params[:id])
       user.update(user_params)
